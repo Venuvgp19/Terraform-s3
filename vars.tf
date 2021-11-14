14a3cadfcdcd7544362110191ed2b75d7bd6f5c8
Venugopal <venugopal.p@unfi.com>
2021-11-14 10:53:02 -0600

    variables defined

--- vars.tf
+++ vars.tf
@@ -3,8 +3,13 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
     default = "ap-south-1"
 }
-variable "prefix" {}
+variable "prefix" {
+    description = "Add prefix for naming your bucket"
+}
 variable "userids" {
+    description = "List of 12 Digit Account IDs to share bucket with ex - ["123456789012"]
     type = list(string)
 }
-variable "log-bucket-id" {}
+variable "log-bucket-id" {
+    description = "Logs of your bucket goes here"
+}
